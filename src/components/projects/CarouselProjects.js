5bd5b421d9423853acf6c51ef6cf1dbffb3a2217
Anthony Pernia <AnthonyPerniaH@gmail.com>
2023-03-31 20:32:30 -0300

    update actions

--- src/components/projects/CarouselProjects.js
+++ src/components/projects/CarouselProjects.js
@@ -9,21 +9,30 @@ const CarouselProjects = () => {
   const [projects, setProjects] = useState([]);
 
   const getProjectsFromApi = async () => {
-    const res = await fetch("https://api.github.com/search/repositories?q=topic:portfolio+user:anthonypernia&sort=updated&order=desc");
+    const res = await fetch("https://api.github.com/search/repositories?q=topic:portfolio-anthony+user:anthonypernia&sort=updated&order=desc");
     const data = await res.json();
     saveProjectsInLocalStorage(data.items);
   };
 
   const saveProjectsInLocalStorage = (projects) => {
     localStorage.setItem("projects", JSON.stringify(projects));
+    localStorage.setItem("lastUpdate", Date.now());
     setProjects(projects);
   };
 
   const getProjectsFromLocalStorage = () => {
     const projects = JSON.parse(localStorage.getItem("projects"));
-    if (projects) {
-      setProjects(projects);
-      return projects;
+    const lastUpdate = localStorage.getItem("lastUpdate");
+    if (projects && lastUpdate) {
+      const diffTime = Math.abs(Date.now() - lastUpdate);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      if (diffDays < 3) {
+        return projects;
+      } else {
+        localStorage.removeItem("projects");
+        localStorage.removeItem("lastUpdate");
+        return null;
+      }
     }
   };
 
@@ -32,6 +41,8 @@ const CarouselProjects = () => {
     const projects = getProjectsFromLocalStorage();
     if (!projects) {
       getProjectsFromApi();
+    }else{
+      setProjects(projects);
     }
   };
 
@@ -63,6 +74,7 @@ const CarouselProjects = () => {
     <div className={styles.CarouselMainContainer} id="PROJECTS">
       <div className={styles.CarouselTitleContainer}>
         <h2 className={styles.CarouselTitle}>Projects</h2>
+        <h3 className={styles.CarouselSubtitle}>These are some of the projects I have developed to automate simple or complex tasks with python or other languages and frameworks.</h3>
       </div>
       
       <div className={styles.CarouselContainer} ref={carouselRef}>
@@ -72,11 +84,11 @@ const CarouselProjects = () => {
       </div>
       <div className={styles.CarouselButton}>
         <button className={styles.CarouselButtonPrev}
-          onClick={() => handleScroll(-80)}
+          onClick={() => handleScroll(-100)}
         >
         </button>
         <button className={styles.CarouselButtonNext}
-          onClick={() => handleScroll(80)}
+          onClick={() => handleScroll(100)}
         >
         </button>
       </div>
